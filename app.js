2e43ad4b826c8ba23c47a12f542a94976f157694
unknown <theodoreonyejiaku@gmail.com>
2020-11-13 13:54:53 +0100

    initialize project

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+//configuring dotenv
+require("dotenv").config();
+//Importing the body-parser middle ware
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+app.use(cors());
+//Use of cookies
+app.use(cookieParser());
+
+app.use(express.static("public"))
+
+var mongoDB = process.env.DB;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connections;
+db.concat("error", console.error.bind(console, "MongoDB connection error."));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log("Server running succesfully on port:"+PORT)
+})
